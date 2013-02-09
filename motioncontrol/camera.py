fd65bf678878be3ba2da7bd1d0b2474922cb9cf7
Matt Belhorn <matt.belhorn@gmail.com>
2013-02-08 20:27:04 -0500

    Camera scans for an additional line candidate to ensure no broken output lines.

--- motioncontrol/camera.py
+++ motioncontrol/camera.py
@@ -8,30 +8,30 @@ class LaserBeamProfiler(object):
   """
   Provides an interface to a Newport HD-LBP over serial link.
   """
-  
+
   def __init__(self, device):
     """
     Establish serial communication with an HD-LBP.
     """
     self.device = device
     self.io = serial.Serial(device, 57600, timeout=1)
-    self.keys = ['time', 'centroid_x', 'centroid_y', 'centroid_r', 
+    self.keys = ['time', 'centroid_x', 'centroid_y', 'centroid_r',
                  'level_1', 'level_2', 'level_3',
                  'width_1', 'width_2', 'width_3',
                  'height_1', 'height_2', 'height_3',
                  'power']
-  
+
   def read(self):
     """
     Read the latest recorded data.
-    
+
     The output is a dictionary that contains the following quantities:
       'time' - Time since camera reset of measurement (seconds)
       'power' - Power deposited on CCD (mW). Requires calibration for accuracy.
       'centroid_x' - Centroid horizontal position from center (micrometers)
       'centroid_y' - Centroid vertical position from center (micrometers)
       'centroid_r' - Image radius (micrometers)
-      
+
       The following sizes of the x-,y-projected image are also given. See
       HD-LBP documentation for more information.
       'level_1' - Projection level 1 (13.5%)
@@ -49,11 +49,10 @@ class LaserBeamProfiler(object):
       buffer = buffer + self.io.read(self.io.inWaiting())
       if ' \n' in buffer:
         lines = buffer.split(' \n')
-        if len(lines) > 1:
+        if len(lines) > 2:
           last_full_line = lines[-2]
           header, values = last_full_line.split(" ",1)
           floats = [float(x) for x in values.split()]
           if len(floats) == 14:
             output = dict(zip(self.keys, floats))
             return output
-        buffer = lines[-1]
