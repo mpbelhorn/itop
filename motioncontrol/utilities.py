1081591b781be2a0748af82a902da4d9f92eee39
Matt Belhorn <matt.belhorn@gmail.com>
2013-03-16 00:17:08 -0400

    Many changes implemented.
    Renamed findSlope to findTrajectory.
    Catching TypeErrors in position along trajectory.
    Initial reorganization of FocalPoint class.

--- motioncontrol/utilities.py
+++ motioncontrol/utilities.py
@@ -48,10 +48,10 @@ class ConstrainToBeam(object):
     self.lower_limit_z = kwargs.pop('lower_limit_z', -125)
     self.upper_limit_z = kwargs.pop('upper_limit_z',  125)
     self.power_level = kwargs.pop('power_level', 0.003)
-    self.r_initial = array([0, 0])
-    self.r_final = array([0, 0])
-    self.slope = array([0, 0])
-    self.slope3D = array([0, 0, 0]) # (x,y,z)
+    self.r_initial = None
+    self.r_final = None
+    self.slope = None
+    self.slope3D = None # (x,y,z)
 
   def centerBeam(self):
     """
@@ -152,7 +152,7 @@ class ConstrainToBeam(object):
     self.centerBeam()
     return self.controller.groupPosition(self.group_id)
 
-  def findSlope(self):
+  def findTrajectory(self):
     """
     Find trajectory of single beam.
     """
@@ -191,10 +191,11 @@ class ConstrainToBeam(object):
     Returns the XZ coordinates of the stage group at given fraction of the
     trajectory.
     """
-    if fraction < 0 or fraction > 1:
-      print "Cannot exceed stage limits."
-    else:
-      return (self.r_initial + fraction * self.slope).tolist()
+    try:
+      return (self.r_initial + clamp(fraction, 0, 1) * self.slope).tolist()
+    except TypeError:
+      print "Trajectory is not initilized"
+      return None
 
   def moveOnBeam(self, fraction):
     self.controller.groupMoveLine(self.group_id,
@@ -208,134 +209,43 @@ class ConstrainToBeam(object):
     return math.atan(self.slope[0] / self.slope[1])
 
 class FocalPoint(object):
+  """
+  Finds, calculates and returns information about the focal point
+  and beam crossing positions in general of two beams.
+  """
   def __init__(self, controller, group_id, camera, **kwargs):
     self.controller = controller
     self.mirror = self.controller.axis1
     self.group_id = group_id
     self.camera = camera
-    self.trajectory = ConstrainToBeam(
-        self.controller, self.group_id, self.camera)
-    self.beam_crossing_found = False
+
+    self.beam_a = ConstrainToBeam(self.controller, self.group_id, self.camera)
+    self.beam_b = ConstrainToBeam(self.controller, self.group_id, self.camera)
+
     self.lower_limit_x = kwargs.pop('lower_limit_x', -125)
     self.upper_limit_x = kwargs.pop('upper_limit_x',  125)
     self.lower_limit_z = kwargs.pop('lower_limit_z', -125)
     self.upper_limit_z = kwargs.pop('upper_limit_z',  125)
     self.power_level = kwargs.pop('power_level', 0.003)
-    self.slope = array([0, 0])
-    self.r_focal = array([0, 0])
 
-
-  def findFocalPoint(self):
-    self.mirror.on()
-    # Block the free beam. Done manually for now. TODO: get an automatic block.
-    unblockedPos = 50
-    blockedPos = 0
-    self.mirror.acceleration(50)
-    self.mirror.velocity(30)
-    self.mirror.position(blockedPos)
-    pauseForStage(self.mirror)
-
-    self.trajectory.findSlope()
-    self.slope = self.trajectory.slope
-
-    # Unblock the free beam. Done manually for now.
-    self.mirror.position(unblockedPos)
-    pauseForStage(self.mirror)
-
-    self.controller.groupVelocity(self.group_id, 5)
-    #
-    # Move cam to focal point
-    # 1: move along beam
-    # 2: find power spike
-    # 3: refine position
-    self.r_focal = array(self.findBeam2(self.slope))
-
-  def searchAlongBeam(self, start_point, stop_point, step_size):
+  def findTrajectories(self):
     """
-    Searches through a range of position steps for the beam.
-
-    All arguments given in millimeters.
+    Initilizes the trajectories of both beams.
     """
-    beam_seen = False
-    x_start = clamp(start_point[0], self.lower_limit_x, self.upper_limit_x)
-    x_stop = clamp(stop_point[0], self.lower_limit_x, self.upper_limit_x)
-    z_start = clamp(start_point[1], self.lower_limit_z, self.upper_limit_z)
-    z_stop = clamp(stop_point[1], self.lower_limit_z, self.upper_limit_z)
-    displacement = math.hypot((x_stop - x_start), (z_stop - z_start))
-    if displacement == 0:
-            return None
-    while displacement < step_size:
-            step_size = abs(float(step_size)) / 2
-    count = int(math.floor(displacement/abs(float(step_size))))
-    x_step = (x_stop - x_start) / float(count)
-    z_step = (z_stop - z_start) / float(count)
-    x_steps = [x_start + x_step*i for i in xrange(count)]
-    z_steps = [z_start + z_step*i for i in xrange(count)]
-    steps = map(list, zip(x_steps, z_steps))
-    steps.append([x_stop, z_stop])
-    for position in steps:
-      self.controller.groupMoveLine(self.group_id, position)
-      while self.controller.groupIsMoving(self.group_id):
-        if (self.camera.read()['power'] > self.power_level):
-          ##
-          #define new power level based on level of a single beam
-          ##
-          beam_seen = True
-      cam_reading = self.camera.read()
-      if (cam_reading['power'] < self.power_level and beam_seen):
-        print "Passed the beam."
-        return position
-      elif (cam_reading['power'] > self.power_level):
-        beam_seen = True
-        beam_offset = cam_reading['centroid_x'] - (x_step * 500.)
-        if (x_step > 0 <= beam_offset) or (x_step < 0 >= beam_offset):
-          print "Passed the beam."
-          return position
-    else:
-      print "Something's not right..."
-      cam_reading = self.camera.read()
-      if cam_reading['power'] > self.power_level:
-        if -20 < cam_reading['centroid_x'] < 20:
-          print "On the beam within thermal fluctuations."
-          return self.controller.groupPosition(self.group_id)
-        elif beam_seen:
-          print "ERROR: Beam center not in reach of camera center."
-        else:
-          # The beam was not detected! The beam may be out of range, blocked, or
-          # the stage moved too fast to register the beam on camera over with
-          # the given serial polling frequency. Also, there may be a bug in the
-          # code.
-          print "ERROR: Beam not detected."
-          self.controller.groupOff(self.group_id)
-          return [0, 0]
+    # Block beam 'B' and find beam 'A' trajectory.
+    raw_input("Block beam 'B'. Press any key to continue.")
+    self.beam_a.findTrajectory()
+    # Block beam 'A' and find beam 'B' trajectory.
+    raw_input("Block beam 'A'. Press any key to continue.")
+    self.beam_b.findTrajectory()
 
-  def findBeam2(self, slope):
+  def findFocalPoints(self):
     """
-    Centers the beam on a camera attached to given stage group.
+    Finds the focal points (assuming astigmatism) of the beams.
     """
-    self.controller.groupVelocity(self.group_id, 30)
-    start_point = self.controller.groupPosition(self.group_id)
-    self.controller.pauseForGroup(self.group_id)
-    time.sleep(1)
-    self.controller.groupVelocity(self.group_id, 5)
-    scan_steps = [50.00, 25.00, 5.00, 1.00, 0.25, 0.12, 0.05, 0.01]
-    scan_range_x = slope[0]
-    scan_range_z = slope[1]
-    for step_number, step_size in enumerate(scan_steps):
-      sign = (-1)**step_number
-      #stop_point = map(sum, zip(start_point, [sign*scan_range, 0]))
-      stop_point = map(sum, zip(start_point, [sign*scan_range_x, sign*scan_range_z]))
-      #start_point = self.search(start_point, stop_point, step_size)
-      start_point = self.searchAlongBeam(start_point, stop_point, step_size)
-      scan_range_x = 2.0 * step_size
-      scan_range_z = 2.0 * step_size
-    return self.controller.groupPosition(self.group_id)
-
-  def outputInfo(self):
-    #grab all variables to output
-    #print "temp output"
-    #give the outut a name (ex A=output) and save to a file
-    #f = open (pathname)
-    #f.write ('string\n')
-    #f.close()
-    return 0
+    # Initialize the beam trajectories if necessary.
+    if self.beam_a.slope3D is None:
+      pass
+    # Find the sagittal focal plane.
+    # Find the meridional focal plane.
+    # Find the circle of least confusion.
