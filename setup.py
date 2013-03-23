8b90af846bda4e4b8b39f05db7af6fcf8a9bf227
Matt Belhorn <matt.belhorn@gmail.com>
2013-03-22 22:14:03 -0400

    Changes to package distribution.

--- setup.py
+++ setup.py
@@ -1,17 +1,23 @@
 from distutils.core import setup
 
 setup(
-    name='MotionControl',
+    name='itop',
     version='0.1.1',
     author='M.P. Belhorn',
     author_email='matt.belhorn@gmail.com',
-    packages=['motioncontrol'],
+    packages=[
+        'itop',
+        'itop.beam',
+        'itop.math',
+        'itop.motioncontrol',
+        'itop.utilities'],
     scripts=[],
     url='https://github.com/emmpbee/ucbelle-optics',
     license='LICENSE.txt',
-    description='EPS300 based stage control.',
+    description='A package to test iTOP mirrors and prisms at UC.',
     long_description=open('README.txt').read(),
     install_requires=[
         "pyserial >= 2.6",
+        "numpy >= 1.7.0",
     ],
 )
