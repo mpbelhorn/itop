8e3866875f7853b8c4af41edf2a26bee33acb853
Matt Belhorn <matt.belhorn@gmail.com>
2013-06-28 19:19:07 -0400

    Added top level function to return a path to data storage directory.

--- itop/__init__.py
+++ itop/__init__.py
@@ -19,8 +19,18 @@ Using any of these subpackages requires an explicit import.
  motioncontrol             --- Interface to EPS30x Stages
  utilities                 --- Misc. funtions.
 """
+
 from itop.beam.focus import FocalPoint
 from itop.beam.profiler import Profiler
 from itop.beam.profiler import Tracker
 from itop.motioncontrol.controller import StageController
+import os
+
+_ROOT = os.path.abspath(os.path.dirname(__file__))
+def data_path(path):
+      path = os.path.join(_ROOT, 'data', path)
+      if os.path.exists(path):
+        return path
+      else:
+        return None
 
