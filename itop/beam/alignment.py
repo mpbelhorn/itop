d63d8c9ecdd2f192da5b53dee2dcd9876ae1d99a
Matt Belhorn <matt.belhorn@gmail.com>
2013-09-24 14:23:05 -0400

    Improved data management in calibration class. Data can now be saved to or recalled from ini-style configuration files.

--- itop/beam/alignment.py
+++ itop/beam/alignment.py
@@ -3,6 +3,10 @@
 A class to manage the alignment of beams to the tracker.
 
 """
+import ConfigParser
+import ast
+import os
+
 from itop.beam import Beam
 from itop.math import Vector
 from itop.utilities import clamp
@@ -117,43 +121,123 @@ class Alignment(object):
     input_x = self.input_positions(mirror_position)[beam_index][0].value
     if (abs(input_x) < Alignment.INTERFERENCE_CUTOFF):
       return True
-    elif (input_x < self.calibration.mirror_dimensions[0][0]) or (
-          self.calibration.mirror_dimensions[0][1] < input_x):
+    elif (input_x < Calibration.EXTENTS['mirror'][0][0]) or (
+          Calibration.EXTENTS['mirror'][0][1] < input_x):
       return True
     else:
       return False
 
 
 class Calibration(object):
-  """Data required to calibrate the instrumentation."""
-  def __init__(self,
-      tcal_from_mcal=(0, 0, 1447),
-      optical_axis_from_mcal=(-33.22, 0.0, -10.832),
-      ccd_from_tcal=(-36.03, 0.0, -3.65),
-      tcal_coordinates=(125.0, 0.0, -95.0),
-      mcal_coordinates=(-220, 0, 0),
-      mirror_dimensions=((-225.0, 225.0), (-20, 0), (0,-100))):
+  """Data required to calibrate the instrumentation.
+
+  The data is divided into three sections. Calibration displacements that do
+  not change between fixed mirror and beam orientations are treated as
+  constants. Calibration displacements that do change between mirrors or
+  beam positions are treated as variables. Lastly, the approximate physical
+  extents of the optical elements are stored as constants. The extents data
+  is not intended to be accurate, but exists only to consolidate the
+  nominal dimensions in a single place for loose logic decisions made
+  in other modules which require that information.
+
+  See the note M.Belhorn "iTOP Mirror Testing" under the section
+  Mirror-Tracker Calibration for more details.
+
+  Default values for each data section are stored in class property
+  dictionaries, and are on the order of actual values.
+
+  Instance copies of the data share the same keys as the defaults. Instance
+  values are loaded from an INI-style configuration file at startup. The
+  displacement data must be in the form of an itop.Vector and appears in the
+  configuration files in the format
+
+      "key = [x, y, z]|[(-ex,+ex), (-ey,+ey), (-ez,+ez)]"
+
+  where 'key' is the name of the displacement; 'x','y','z' are the coordinates
+  of the vector; and 'ex', 'ey', and 'ez' are the measurement uncertainties.
+
+  """
+  CONSTANTS = {
+      'calibration_mirror':   Vector([-120.000, 0.000,    0.000], 0.0005),
+      'calibration_tracker':  Vector([-125.000, 0.000,   95.000], 0.0005),
+      'registration_mirror':  Vector([ 225.000, 0.000,    0.000], 0.001),
+      'registration_tracker': Vector([ -32.004, 0.000,    6.600], 0.01),
+      'reference_tracker':    Vector([  -4.026, 0.000,  -10.250], 0.01),
+      'reference_delta':      Vector([   0.000, 0.000, 1447.000], 0.8),
+      }
+
+  VARIABLES = {
+      'reference_mirror':Vector([33.220, 0.000, 10.832], 0.01),
+      }
+
+  EXTENTS = {
+    'mirror':[(-225, 225), (-20, 0), (-100, 0)]
+    }
+
+  def __init__(self, configuration=None):
     """
     Constructs a calibration object.
 
     """
-    # mirror_faces - mirror_mount = (-11.61, 0.0, -8.882)
-    # mirror_mount - mcal = (-21.61, 0.0, -1.95)
-    # ccd - profiler_chassis = (-32.004, 0.0, 6.60)
-    #
-    self.tcal_from_mcal = Vector(tcal_from_mcal)
-    self.optical_axis_from_mcal = Vector(optical_axis_from_mcal)
-    self.tcal_coordinates = Vector(tcal_coordinates)
-    self.mcal_coordinates = Vector(mcal_coordinates)
-    self.mirror_dimensions = mirror_dimensions
+    self._path = configuration
+    self.data = {}
+    if configuration is not None:
+      self.load(configuration)
+    else:
+      self.data.update(Calibration.CONSTANTS)
+      self.data.update(Calibration.VARIABLES)
+
+  def __repr__(self):
+    return 'Calibration({})'.format(
+        'default' if self._path is None else os.path.basename(self._path))
+
+  def displacement(self):
+    """Return the vector displacement of the tracker origin from the mirror
+    optical axis at the mirror stage system origin."""
+    return sum((i for k, i in self.data.iteritems() if k in self.data))
 
   def primary_input(self):
     """Return the input position of the primary beam when the mirror stage
     is at position (0, 0).
     """
-    return Vector([0.0, 0.0, 0.0])
+    return self.data['calibration_mirror'] + self.data['registration_mirror']
 
-  def offset(self):
-    # TODO - Nonzero y is sign-reversed in the calculation.
-    return -self.tcal_coordinates + self.tcal_from_mcal - self.optical_axis_from_mcal
+  def save(self, path):
+    """Save the calibration data to the given path."""
+    config = ConfigParser.RawConfigParser()
+    config.add_section('Constants')
+
+    config.add_section('Variables')
+    for key in Calibration.VARIABLES:
+      _save_vector(config, 'Variables', key, self.data[key])
+
+    for key in Calibration.CONSTANTS:
+      _save_vector(config, 'Constants', key, self.data[key])
+
+    with open(path, 'wb') as configfile:
+      config.write(configfile)
+    self._path = path
+
+  def load(self, path):
+    """Load calibration data from the given path."""
+    config = ConfigParser.RawConfigParser()
+    config.read(path)
+    self._path = path
+
+    for key in Calibration.VARIABLES:
+      self.data[key] = _load_vector(config, 'Variables', key)
+
+    for key in Calibration.CONSTANTS:
+      self.data[key] = _load_vector(config, 'Constants', key)
+
+
+def _load_vector(config, section, key):
+  """Return a vector from a configuration file."""
+  data = [ast.literal_eval(i) for i in config.get(section, key).split('|')]
+  return Vector(data[0], [data[1]])
+
+def _save_vector(config, section, key, item):
+  """Return a vector from a configuration file."""
+  entry = '{}|{}'.format(item.array().tolist(), item.errors())
+  config.set(section, key, entry)
 
