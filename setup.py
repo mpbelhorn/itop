eaa15cb6cfaca7bd07d352e4e5468123728fb405
Matt Belhorn <matt.belhorn@gmail.com>
2014-02-13 10:38:55 -0500

    Updated package setup

--- setup.py
+++ setup.py
@@ -1,23 +1,37 @@
-from distutils.core import setup
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
 
 setup(
     name='itop',
-    version='0.2.0',
-    author='M.P. Belhorn',
+    version='0.6.0',
+    author='M.P. Belhorn et al.',
     author_email='matt.belhorn@gmail.com',
+    maintainer='M.P. Belhorn',
+    maintainter_email='matt.belhorn@gmail.com',
     packages=[
         'itop',
+        'itop.analyis',
         'itop.beam',
         'itop.math',
         'itop.motioncontrol',
-        'itop.utilities'],
-    scripts=[],
-    url='https://github.com/emmpbee/ucbelle-optics',
+        'itop.photodiode',
+        'itop.raytrace',
+        'itop.utilities',
+        ],
+    scripts=[
+        'bin/scan-mirror',
+        'bin/measure-tilt',
+        ],
+    url='https://github.com/emmpbee/itop',
     license='MIT',
-    description='A package to test iTOP mirrors and prisms at UC.',
+    description='Suite for testing iTOP mirrors and prisms at the University of Cincinnati.',
     long_description=open('README.txt').read(),
+    include_package_data=True,
     install_requires=[
         "pyserial >= 2.6",
         "numpy >= 1.7.0",
+        "matplotlib >= 1.3.1",
     ],
 )
