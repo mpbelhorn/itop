a6f5188e7d42a58c6d5e8f6ea4b78e12a33d4e8c
Matt Belhorn <matt.belhorn@gmail.com>
2014-02-24 11:14:35 -0500

    Improved prism angle function symantics. Added utility functions for prism testing including a means of calculating the angle directly from a pair of beam objects.

--- itop/prism/__init__.py
+++ itop/prism/__init__.py
@@ -2,13 +2,25 @@
 A package for handeling prism testing.
 """
 
-from math import sin, cos, atan
+from math import sin, cos, acos, atan, degrees
 
-def angle(alpha, index_quartz=1.4608, index_air=1.000277):
+def deflection_angle(undeflected_beam, deflected_beam):
+  """Return the angle of beam deflection in radians."""
+  return acos(undeflected_beam.direction.dot(deflected_beam.direction).value)
+
+def prism_angle(alpha, index_quartz=1.4608, index_air=1.000277):
   """Return the prism angle given the beam's angle of deflection
   when the prism is moved into the beam path.
 
   """
   return atan(sin(alpha) / ((index_quartz / index_air) - cos(alpha)))
 
+def angle_from_beams(undeflected_beam, deflected_beam, **kwargs):
+  """Return the prism angle in degrees from a pair of deflected and
+  undeflected beam trajectories.
 
+  Any keyword arguments are passed to the deflection angle calculation
+  itop.prism_angle().
+  """
+  return degrees(prism_angle(
+      deflection_angle(undeflected_beam, deflected_beam), **kwargs))
